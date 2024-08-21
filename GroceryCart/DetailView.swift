edab4b5b06cce248313a2618cffa00b692067372
charline2020 <p78071044@gs.ncku.edu.tw>
2024-08-21 17:07:54 -0500

    build History page outline

--- GroceryCart/DetailView.swift
+++ GroceryCart/DetailView.swift
@@ -1,6 +1,6 @@
 //
 //  DetailView.swift
-//  Cart
+//  GroceryCart
 //
 //  Created by Charline on 2024/8/21.
 //
@@ -8,63 +8,11 @@
 import SwiftUI
 
 struct DetailView: View {
-    var selectedItems: [String] // List of selected items
-    var missingItems: [String] // List of selected items
-
     var body: some View {
-                
-        VStack {
-            if selectedItems.isEmpty && missingItems.isEmpty{
-                Text("No items purchased.")
-                    .font(.system(size: 24))
-                    .padding()
-                
-            } else {
-                Text("Purchased")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-
-                List {
-                    ForEach(Array(selectedItems.enumerated()), id: \.offset) { _, item in
-                        Text("\(item)") // Numbering each item
-                            .padding(.leading)
-                            .font(.system(size: 20))
-                    }
-                }
-                .background()
-                .cornerRadius(8)
-                .shadow(radius: 1) // Optional: Add shadow for depth
-                .padding()
-                
-                Text("Missed Items")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                
-                List {
-                    ForEach(Array(missingItems.enumerated()), id: \.offset) { _, item in
-                        Text("\(item)") // Numbering each item
-                            .padding(.leading)
-                            .font(.system(size: 20))
-                    }
-                }
-                .background()
-                .cornerRadius(8)
-                .shadow(radius: 1) // Optional: Add shadow for depth
-                .padding()
-            }
-
-            
-        }
-
-        .navigationTitle("Home")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DetailView(selectedItems: ["Apples", "Bananas", "Carrots"], missingItems:["Oranges", "Eggs"])
-        .frame(width: 400, height: 600)
+    DetailView()
 }
