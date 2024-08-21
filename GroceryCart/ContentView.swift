f93cec9fb3e99ddc544e1121bf8f863041d713c1
charline2020 <p78071044@gs.ncku.edu.tw>
2024-08-21 16:57:28 -0500

    Initial Commit

---
+++ GroceryCart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryCart
+//
+//  Created by Charline on 2024/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryCart/GroceryCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryCartApp.swift
+//  GroceryCart
+//
+//  Created by Charline on 2024/8/21.
+//
+
+import SwiftUI
+
+@main
+struct GroceryCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
