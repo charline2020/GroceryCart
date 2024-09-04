c86dba7470175ba0e2d0bad211f5f35e8ea90ffa
charline2020 <p78071044@gs.ncku.edu.tw>
2024-09-04 10:57:44 -0500

    add alert when same grocery item is added

---
+++ GroceryCart/GroceryRepeatAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryRepeatAlertView.swift
+//  GroceryCart
+//
+//  Created by Charline on 2024/9/4.
+//
+
+import SwiftUI
+
+struct GroceryRepeatAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroceryRepeatAlertView()
+}
